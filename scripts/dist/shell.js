f4e829f1a8d378344554d60707121abe355c11b8
Emanuel <emanuel.palestino.h@gmail.com>
2021-12-15 21:21:18 -0600

    Palabras reservadas corregidas y color para #numero arreglado

--- scripts/dist/shell.js
+++ scripts/dist/shell.js
@@ -20,15 +20,10 @@ CodeMirror.defineMode('shell', function() {
     }
   };
 
-  var commonAtoms = ["true", "false"];
-  var commonKeywords = ["if", "then", "do", "else", "elif", "while", "until", "for", "in", "esac", "fi",
-    "fin", "fil", "done", "exit", "set", "unset", "export", "function"];
-  var commonCommands = ["ab", "awk", "bash", "beep", "cat", "cc", "cd", "chown", "chmod", "chroot", "clear",
-    "cp", "curl", "cut", "diff", "echo", "find", "gawk", "gcc", "get", "git", "grep", "hg", "kill", "killall",
-    "ln", "ls", "make", "mkdir", "openssl", "mv", "nc", "nl", "node", "npm", "ping", "ps", "restart", "rm",
-    "rmdir", "sed", "service", "sh", "shopt", "shred", "source", "sort", "sleep", "ssh", "start", "stop",
-    "su", "sudo", "svn", "tee", "telnet", "top", "touch", "vi", "vim", "wall", "wc", "wget", "who", "write",
-    "yes", "zsh", "add", "sub", "addi", "subi", "stur", "ldur", "ADD"];
+  var commonAtoms = [];
+  var commonKeywords = ["x", "X"];
+  var commonCommands = ["add", "sub", "addi", "subi", "stur", "ldur", "lsl", "lsr", "b", "bl", "br", "cbnz", "cbz",
+                        "ADD", "SUB", "ADDI", "SUBI", "STUR", "LDUR", "LSL", "LSR", "B", "BL", "BR", "CBNZ", "CBZ"];
 
   CodeMirror.registerHelper("hintWords", "shell", commonAtoms.concat(commonKeywords, commonCommands));
 
@@ -55,6 +50,12 @@ CodeMirror.defineMode('shell', function() {
         stream.skipToEnd();
         return 'meta'; // 'comment'?
       }
+      if (sol && stream.eatWhile(/\d/)) {
+        if(stream.eol() || !/\w/.test(stream.peek())) {
+        return 'number';
+      }
+      }
+      // Cuando se detecto solo el #
       stream.skipToEnd();
       return 'comment';
     }
